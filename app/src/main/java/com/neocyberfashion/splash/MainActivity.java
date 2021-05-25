46f4640cea481ddab92c72c22be2c12a4a86eebd
Amimul <amimulahsan7@gmail.com>
2021-05-25 22:36:32 +0600

    Added welcome screen.....

--- app/src/main/java/com/neocyberfashion/splash/MainActivity.java
+++ app/src/main/java/com/neocyberfashion/splash/MainActivity.java
@@ -5,10 +5,14 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.TextView;
 import com.neocyberfashion.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int state = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +20,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Change status bar color
-        getWindow().setStatusBarColor(Color.parseColor("#4E0F0E"));
-        getWindow().setNavigationBarColor(Color.parseColor("#000000"));
+//        getWindow().setStatusBarColor(Color.parseColor("#4E0F0E"));
+//        getWindow().setNavigationBarColor(Color.parseColor("#000000"));
+
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
 
         TextView textView = findViewById(R.id.start_shopping);
 
         textView.setOnClickListener(v -> {
+            state = 1;
             Intent intent = new Intent(MainActivity.this, WelcomeScreen1.class);
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
@@ -30,12 +39,15 @@ public class MainActivity extends AppCompatActivity {
         });
 
         new Handler().postDelayed(() -> {
-            Intent mainIntent = new Intent(MainActivity.this, WelcomeScreen1.class);
-            startActivity(mainIntent);
-            mainIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            mainIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            finish();
-        }, 3000);
+            if(state == 0)
+            {
+                Intent mainIntent = new Intent(MainActivity.this, WelcomeScreen1.class);
+                startActivity(mainIntent);
+                mainIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                mainIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                finish();
+            }
+        }, 4000);
 
     }
 }
--- app/src/main/java/com/neocyberfashion/splash/WelcomeScreen1.java
+++ app/src/main/java/com/neocyberfashion/splash/WelcomeScreen1.java
@@ -1,8 +1,9 @@
 package com.neocyberfashion.splash;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 import com.neocyberfashion.R;
 
@@ -12,5 +13,10 @@ public class WelcomeScreen1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen1);
+
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
     }
 }
